e5bbf4d5ad08b7139488938fab42290afbd0d8c5
Kevin Castro <cas20057@byui.edu>
2024-03-29 16:58:10 -0600

    created basic Todos page, and set up routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+/**
+ * This will retry failed chunks up to 5 times
+ * @param {Function} lazyComponent - lazy component import function
+ * @param {Number} attemptsLeft - number of attempts to make (defaults to 5)
+ * @returns {Promise<any>} Promise that rejects if all additional attempts fail.
+ */
+function componentLoader(lazyComponent, attemptsLeft = 5) {
+  return new Promise((resolve, reject) => {
+    lazyComponent()
+      .then(resolve)
+      .catch((error) => {
+        // let us retry after 100 ms
+        setTimeout(() => {
+          if (attemptsLeft === 1) {
+            reject(error);
+            return;
+          }
+          componentLoader(lazyComponent, attemptsLeft - 1).then(
+            resolve,
+            reject
+          );
+        }, 100);
+      });
+  });
+}
+
+// Lazy load imports
+const TodosPage = lazy(() =>
+  componentLoader(() => import('./pages/todos/TodosPage'))
+);
+
 function App() {
-  return <div className='app'>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <Suspense fallback={<h1>Loading...</h1>}>
+              <TodosPage />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/todos/TodosPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/**
+ * Displays the Todos page, which contains a list of todos separated by status
+ * @returns {JSX.Element}
+ */
+function TodosPage() {
+  return <div>Todos Page</div>;
+}
+
+export default TodosPage;
