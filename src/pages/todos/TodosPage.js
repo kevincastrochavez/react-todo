b868e7018dab7a32d808df1200d04d5c0e54bab9
Kevin Castro <cas20057@byui.edu>
2024-03-29 17:15:45 -0600

    installed emotion core for css-in-js styling

--- src/pages/todos/TodosPage.js
+++ src/pages/todos/TodosPage.js
@@ -1,11 +1,20 @@
-import React from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const mainContainerCSS = css`
+  padding: 30px 20px;
+`;
 
 /**
  * Displays the Todos page, which contains a list of todos separated by status
  * @returns {JSX.Element}
  */
 function TodosPage() {
-  return <div>Todos Page</div>;
+  return (
+    <main css={mainContainerCSS}>
+      <h1>All Todos</h1>
+    </main>
+  );
 }
 
 export default TodosPage;
