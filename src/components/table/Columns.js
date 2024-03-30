dffad83137b981251ee8a67657dd814bc49a428c
Kevin Castro <cas20057@byui.edu>
2024-03-29 18:30:30 -0600

    modified columns definition so it accepts a checkbox

--- src/components/table/Columns.js
+++ src/components/table/Columns.js
@@ -1,6 +1,16 @@
-'use client';
+import { Checkbox } from '../checkbox/Checkbox';
 
 export const columns = [
+  {
+    id: 'select',
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label='Select row'
+      />
+    ),
+  },
   {
     accessorKey: 'name',
     header: 'Name',
