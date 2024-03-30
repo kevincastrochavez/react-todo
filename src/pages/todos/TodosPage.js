7b156656a0c9a54d5936654c0121f98d57a75d37
Kevin Castro <cas20057@byui.edu>
2024-03-29 19:09:52 -0600

    imported the datatable component into the page and pulled the info

--- src/pages/todos/TodosPage.js
+++ src/pages/todos/TodosPage.js
@@ -2,7 +2,10 @@
 import { css } from '@emotion/react';
 import { PlusIcon } from '@radix-ui/react-icons';
 
-import { Button } from '../../components/button/button';
+import { columns } from '../../components/table/Columns';
+import { Button } from '../../components/button/Button';
+import { DataTable } from '../../components/table/DataTable';
+import { useTodos } from '../../components/TodosProvider';
 
 const todoPageContainerCSS = css`
   padding: 30px 20px;
@@ -21,20 +24,46 @@ const headerContainerCSS = css`
   }
 `;
 
+const mainContainerCSS = css`
+  margin-top: 30px;
+
+  & h2 {
+    font-size: 24px;
+    margin-bottom: 8px;
+  }
+`;
+
 /**
  * Displays the Todos page, which contains a list of todos separated by status
  * @returns {JSX.Element}
  */
 function TodosPage() {
+  const { pendingTodos, completedTodos } = useTodos();
+
   return (
     <div css={todoPageContainerCSS}>
       <header css={headerContainerCSS}>
         <h1>All Todos</h1>
         <Button>
-          <PlusIcon className='mr-2 h-4 w-4' /> Add Todo
+          <PlusIcon className='mr-2 h-5 w-5' /> Add Todo
         </Button>
       </header>
-      <main></main>
+      <main css={mainContainerCSS}>
+        <h2>Pending</h2>
+        <DataTable
+          columns={columns}
+          data={pendingTodos}
+          emptyTodoText='No Todos are pending at the time'
+          emptyTodoButton='Add Todo'
+        />
+        <h2>Completed</h2>
+        <DataTable
+          columns={columns}
+          data={completedTodos}
+          emptyTodoText='No Todos are completed yet. Add one and complete it!'
+          emptyTodoButton='Add Todo'
+        />
+      </main>
     </div>
   );
 }
